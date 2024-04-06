cf41d4f81e642a9a249a4a0003b009d79d68e1cf
SolFreeman <soloviyov.stas@gmail.com>
2024-04-07 02:41:55 +0300

    Updates

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,19 +1,6 @@
-$(window).on('load resize', function () {
-	if ($(window).width() < 768) {
-		$('.benefis__slider:not(.slick-initialized)').slick({
-			dots: true,
-			infinite: true,
-			speed: 100,
-			slidesToShow: 1,
-			adaptiveHeight: true
-		});
-	} else {
-		$(".benefis__slider.slick-initialized").slick("unslick");
-	}
-});
 jQuery(document).ready(function ($) {
 	faqAccordeon();
-	inputFileCustom();
+	//inputFileCustom();
 	listCategory();
 	tooltip();
 	$(".fancybox").fancybox();
@@ -30,6 +17,69 @@ jQuery(document).ready(function ($) {
 	$(".select_single").select2({
 		theme: 'single'
 	});
+
+	$(window).on('load resize', function () {
+		if ($(window).width() < 768) {
+			$('.benefis__slider:not(.slick-initialized)').slick({
+				dots: true,
+				infinite: true,
+				speed: 100,
+				slidesToShow: 1,
+				adaptiveHeight: true,
+				responsive: [
+					{
+						breakpoint: 768,
+						settings: {
+							arrows: false
+						}
+					},
+				]
+			});
+		} else {
+			$(".benefis__slider.slick-initialized").slick("unslick");
+		}
+	});
+
+	$(window).on('load scroll', function () {
+		var windowHeight = $(window).height();
+		var scrollTop = $(window).scrollTop();
+		var listTop = $('.commission__photo-line').offset().top;
+
+		if (scrollTop + windowHeight > listTop) {
+			var delay = 0;
+			$('.commission__photo').each(function () {
+				$(this).delay(delay).queue(function () {
+					$(this).addClass('animated').dequeue();
+				});
+				delay += 100;
+			});
+		}
+	});
+
+	// Функция для пересчета высоты суб контента
+    function setSubcontentHeight() {
+        if ($(window).width() >= 768) {
+			var height = $('.active > .sub-content').outerHeight();
+			$('.category-holder').height(height);
+		} else {
+			$('.category-holder').height('');
+		}
+    }
+
+    setSubcontentHeight();
+    $(window).resize(function() {
+        setSubcontentHeight();
+    });
+
+    $('.category-holder .item').click(function() {
+        setSubcontentHeight();
+    });
+
+
+	$(window).trigger('resize');
+
+
+
 	modalOpen();
 	if ($(window).width() < 768) {
 		loadMore();
@@ -120,8 +170,16 @@ jQuery(document).ready(function ($) {
 	})
 
 	$('.js-visual-slider').slick({
-		arrows: true,
 		dots: true,
+		adaptiveHeight: true,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					arrows: false
+				}
+			},
+		]
 	});
 
 	$('.feedbacks__slider').slick({
@@ -173,6 +231,193 @@ jQuery(document).ready(function ($) {
 	document.addEventListener('aos:out', ({ detail }) => {
 		console.log('animated out', detail);
 	});
+
+
+	function initSearchFormSwitcher() {
+		jQuery('.btn_search').on('click', function () {
+			jQuery('.search-holder').toggleClass('active');
+		});
+		$(document).mouseup(function (e) {
+			let div = $('.search-holder');
+			if (!div.is(e.target)
+				&& div.has(e.target).length === 0) {
+				div.removeClass('active');
+			}
+		});
+	}
+
+	function resizeBlock() {
+		if (window.matchMedia("screen and (max-width: 1319px)").matches) {
+			$('.btn_pay').appendTo($('.logo-area'));
+		}
+		else if (window.matchMedia("screen and (min-width: 1319px)").matches) {
+			$('.btn_pay').appendTo($('.header .btn-box'));
+		}
+	}
+
+	function loadMore() {
+		var $items = $(".category__item");
+		var count = 3;
+		var visible = 3;
+		var windowWidth = $(window).width();
+
+		function updateVisibility() {
+			if (windowWidth < 768) {
+				$items.slice(visible).hide();
+			} else {
+				$items.show();
+			}
+		}
+
+		updateVisibility();
+
+		$(window).resize(function () {
+			windowWidth = $(window).width();
+			updateVisibility();
+		});
+
+		$(".load_more").click(function (event) {
+			event.preventDefault();
+			$items.slice(visible, visible + count).show();
+			visible += count;
+
+			if (visible >= $items.length) {
+				$(this).hide();
+			}
+		});
+	}
+
+	jQuery(window).resize(function () {
+		resizeBlock();
+		if (jQuery(window).width() < 768) {
+			loadMore();
+		}
+
+
+	});
+
+	function subMenuNavigation() {
+		let doc = jQuery(document),
+			isTouchDevice = /Windows Phone/.test(navigator.userAgent) || ('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch;
+
+		if (isTouchDevice === true) {
+			jQuery('.nav .menu-item-has-children').each(function () {
+				let navItem = jQuery(this).addClass('touch-device'),
+					navItemLink = navItem.children('a'),
+					dataClick = navItem.attr('data-click', 0),
+					subMenu = navItem.children('.sub-menu');
+
+				navItemLink.on('touchstart', function (e) {
+					if (navItem.attr('data-click') == 0) {
+						e.preventDefault();
+
+						navItem.attr('data-click', 1).addClass('open');
+						subMenu.slideDown();
+					} else if (navItem.attr('data-click') == 1) {
+						navItem.attr('data-click', 0).removeClass('open');
+						subMenu.slideUp();
+					}
+				});
+
+				function outsideClickHandler(e) {
+					if (!jQuery(e.target).closest(navItem).length) {
+						navItem.removeClass('open').attr('data-click', 0);
+						subMenu.slideUp();
+					}
+				}
+
+				doc.on('click touchstart', outsideClickHandler);
+			});
+		}
+	}
+
+	function faqAccordeon() {
+		var allLi = jQuery('.faq-list li'),
+			allSub = allLi.children('.filter');
+
+		jQuery('.faq-list li > span').each(function () {
+			var doc = jQuery(document),
+				$this = jQuery(this),
+				item = $this.parent('li'),
+				itemFilter = $this.next('.text-faq'),
+				itemParent = item.parents('li');
+
+
+			$this.on('click', function () {
+				if (item.hasClass('active')) {
+					itemFilter.slideUp();
+					item.removeClass('active');
+				}
+				else {
+					allLi.not(itemParent).removeClass('active');
+					allLi.not(itemParent).find('.text-faq').slideUp();
+					itemFilter.slideDown();
+					item.addClass('active');
+				}
+			});
+		});
+	}
+
+	function listCategory() {
+		var allLi = jQuery('.list-category .item'),
+			allSub = allLi.children('.sub-content');
+
+		jQuery('.list-category .item > .intro-box').each(function () {
+			var doc = jQuery(document),
+				$this = jQuery(this),
+				item = $this.parent('li'),
+				itemFilter = $this.next('.sub-content'),
+				itemParent = item.parents('li');
+
+			$this.on('click', function () {
+				if (jQuery(window).width() < 1024) {
+					if (item.hasClass('active')) {
+						itemFilter.slideUp();
+						item.removeClass('active');
+					} else {
+						allLi.not(itemParent).removeClass('active');
+						allLi.not(itemParent).find('.sub-content').slideUp();
+						itemFilter.slideDown();
+						item.addClass('active');
+					}
+				} else {
+					if (item.hasClass('active')) {
+						itemFilter.fadeOut();
+						item.removeClass('active');
+					} else {
+						allLi.not(itemParent).removeClass('active');
+						allLi.not(itemParent).find('.sub-content').fadeOut();
+						itemFilter.fadeIn();
+						item.addClass('active');
+					}
+				}
+			});
+		});
+	}
+
+	function tooltip() {
+		$('.how-list li').each(function () {
+			var tooltip = $(this).find('.tooltip');
+			var icon = $(this).find('.icon-info');
+			var close = $(this).find('.close');
+
+			// Открытие тултипа при клике на иконку
+			icon.click(function () {
+				tooltip.fadeIn();
+			});
+
+			// Закрытие тултипа при клике на крестик или вне области тултипа
+			$(document).mouseup(function (e) {
+				if (!tooltip.is(e.target) && tooltip.has(e.target).length === 0 && !icon.is(e.target)) {
+					tooltip.fadeOut();
+				}
+				if (close.is(e.target)) {
+					tooltip.fadeOut();
+				}
+			});
+		});
+	}
+
 });
 
 
@@ -241,190 +486,5 @@ function inputFileCustom() {
 			// Повесим функцию onChange на событие change у нового <input>
 			input.addEventListener('change', onChange);
 		}
-	});
-}
-
-function initSearchFormSwitcher() {
-	jQuery('.btn_search').on('click', function () {
-		jQuery('.search-holder').toggleClass('active');
-	});
-	$(document).mouseup(function (e) {
-		let div = $('.search-holder');
-		if (!div.is(e.target)
-			&& div.has(e.target).length === 0) {
-			div.removeClass('active');
-		}
-	});
-}
-
-function resizeBlock() {
-	if (window.matchMedia("screen and (max-width: 1319px)").matches) {
-		$('.btn_pay').appendTo($('.logo-area'));
-	}
-	else if (window.matchMedia("screen and (min-width: 1319px)").matches) {
-		$('.btn_pay').appendTo($('.header .btn-box'));
-	}
-}
-
-function loadMore() {
-	var $items = $(".category__item");
-	var count = 3;
-	var visible = 3;
-	var windowWidth = $(window).width();
-
-	function updateVisibility() {
-		if (windowWidth < 768) {
-			$items.slice(visible).hide();
-		} else {
-			$items.show();
-		}
-	}
-
-	updateVisibility();
-
-	$(window).resize(function () {
-		windowWidth = $(window).width();
-		updateVisibility();
-	});
-
-	$(".load_more").click(function (event) {
-		event.preventDefault();
-		$items.slice(visible, visible + count).show();
-		visible += count;
-
-		if (visible >= $items.length) {
-			$(this).hide();
-		}
-	});
-}
-
-$(window).resize(function () {
-	resizeBlock();
-	if ($(window).width() < 768) {
-		loadMore();
-	}
-
-
-});
-
-function subMenuNavigation() {
-	let doc = jQuery(document),
-		isTouchDevice = /Windows Phone/.test(navigator.userAgent) || ('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch;
-
-	if (isTouchDevice === true) {
-		jQuery('.nav .menu-item-has-children').each(function () {
-			let navItem = jQuery(this).addClass('touch-device'),
-				navItemLink = navItem.children('a'),
-				dataClick = navItem.attr('data-click', 0),
-				subMenu = navItem.children('.sub-menu');
-
-			navItemLink.on('touchstart', function (e) {
-				if (navItem.attr('data-click') == 0) {
-					e.preventDefault();
-
-					navItem.attr('data-click', 1).addClass('open');
-					subMenu.slideDown();
-				} else if (navItem.attr('data-click') == 1) {
-					navItem.attr('data-click', 0).removeClass('open');
-					subMenu.slideUp();
-				}
-			});
-
-			function outsideClickHandler(e) {
-				if (!jQuery(e.target).closest(navItem).length) {
-					navItem.removeClass('open').attr('data-click', 0);
-					subMenu.slideUp();
-				}
-			}
-
-			doc.on('click touchstart', outsideClickHandler);
-		});
-	}
-}
-
-function faqAccordeon() {
-	var allLi = jQuery('.faq-list li'),
-		allSub = allLi.children('.filter');
-
-	jQuery('.faq-list li > span').each(function () {
-		var doc = jQuery(document),
-			$this = jQuery(this),
-			item = $this.parent('li'),
-			itemFilter = $this.next('.text-faq'),
-			itemParent = item.parents('li');
-
-
-		$this.on('click', function () {
-			if (item.hasClass('active')) {
-				itemFilter.slideUp();
-				item.removeClass('active');
-			}
-			else {
-				allLi.not(itemParent).removeClass('active');
-				allLi.not(itemParent).find('.text-faq').slideUp();
-				itemFilter.slideDown();
-				item.addClass('active');
-			}
-		});
-	});
-}
-
-function listCategory() {
-	var allLi = jQuery('.list-category .item'),
-		allSub = allLi.children('.sub-content');
-
-	jQuery('.list-category .item > .intro-box').each(function () {
-		var doc = jQuery(document),
-			$this = jQuery(this),
-			item = $this.parent('li'),
-			itemFilter = $this.next('.sub-content'),
-			itemParent = item.parents('li');
-
-		$this.on('click', function () {
-			if (jQuery(window).width() < 1024) {
-				if (item.hasClass('active')) {
-					itemFilter.slideUp();
-					item.removeClass('active');
-				} else {
-					allLi.not(itemParent).removeClass('active');
-					allLi.not(itemParent).find('.sub-content').slideUp();
-					itemFilter.slideDown();
-					item.addClass('active');
-				}
-			} else {
-				if (item.hasClass('active')) {
-					itemFilter.fadeOut();
-					item.removeClass('active');
-				} else {
-					allLi.not(itemParent).removeClass('active');
-					allLi.not(itemParent).find('.sub-content').fadeOut();
-					itemFilter.fadeIn();
-					item.addClass('active');
-				}
-			}
-		});
-	});
-}
-
-function tooltip() {
-	$('.how-list li').each(function () {
-		var tooltip = $(this).find('.tooltip');
-		var icon = $(this).find('.icon-info');
-		var close = $(this).find('.close');
-
-		// Открытие тултипа при клике на иконку
-		icon.click(function () {
-			tooltip.fadeIn();
-		});
-
-		// Закрытие тултипа при клике на крестик или вне области тултипа
-		$(document).mouseup(function (e) {
-			if (!tooltip.is(e.target) && tooltip.has(e.target).length === 0 && !icon.is(e.target)) {
-				tooltip.fadeOut();
-			}
-			if (close.is(e.target)) {
-				tooltip.fadeOut();
-			}
-		});
 	});
 }
